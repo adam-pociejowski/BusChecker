d83f5a5c6fdde5d913b76dfb0c7c33c72ae2d621
valverde <valverde12345@gmail.com>
2016-11-18 21:03:07 +0100

    Run to heroku changes.

--- src/main/java/com/valverde/buschecker/BusCheckerApplication.java
+++ src/main/java/com/valverde/buschecker/BusCheckerApplication.java
@@ -2,10 +2,14 @@ package com.valverde.buschecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ServletComponentScan
 public class BusCheckerApplication {
 
 	public static void main(String[] args) {
